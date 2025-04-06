e0328d713f2a3828f8101878249d71f7aaf0315c
sushib0mb <azuchitaizo@gmail.com>
2025-04-06 17:13:27 -0400

    Added timetable data

--- jfbfestival/lib/data/timetableData.dart
+++ jfbfestival/lib/data/timetableData.dart
@@ -19,85 +19,320 @@ class Event {
 class EventItem {
   final String title;
   final String time;
-  final String image; // Flutter ではアイコン名や画像パスに置き換え
+  final String image;
+  final int duration;
 
-  EventItem({required this.title, required this.time, required this.image});
+  EventItem({
+    required this.title,
+    required this.time,
+    required this.duration,
+    required this.image,
+  });
 }
 
 class ScheduleItem {
   final String time;
-  final EventItem? stage1Event;
+  final List<EventItem>? stage1Events;
   final EventItem? stage2Event;
 
-  ScheduleItem({required this.time, this.stage1Event, this.stage2Event});
+  ScheduleItem({required this.time, this.stage1Events, this.stage2Event});
 }
 
 final List<ScheduleItem> day1ScheduleData = [
   ScheduleItem(
     time: "11:00 am",
-    stage1Event: EventItem(
-      title: "Opening Ceremony",
-      time: "11:00-11:30",
-      image: "icon_socialdance", // ※実際のアイコンまたは画像パスに変更
+    stage1Events: [
+      EventItem(
+        title: "One Week Wonder",
+        time: "11:00-11:15",
+        duration: 15,
+        image: "",
+      ),
+      EventItem(
+        title: "Showa Boston Dance Performance",
+        time: "11:15-11:30",
+        duration: 15,
+        image: "",
+      ),
+    ],
+    stage2Event: EventItem(
+      title: "set change",
+      time: "11:30-11:35",
+      duration: 5,
+      image: "",
     ),
-    stage2Event: null,
   ),
   ScheduleItem(
     time: "11:30 am",
-    stage1Event: null,
+    stage1Events: [
+      EventItem(
+        title: "NODO JIMAN",
+        time: "11:30-12:00",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Hiroko Watanabe Calligraphy",
+        time: "12:00-12:30",
+        duration: 30,
+        image: "",
+      ),
+    ],
     stage2Event: EventItem(
-      title: "Welcome Performance",
-      time: "11:30-11:45",
-      image: "icon_music_note",
+      title: "ukulele",
+      time: "11:35-12:05",
+      duration: 30,
+      image: "",
     ),
   ),
   ScheduleItem(
     time: "12:00 pm",
-    stage1Event: EventItem(
-      title: "Showa Boston Dance Performance",
-      time: "12:00-12:15",
-      image: "icon_dance",
-    ),
+    stage1Events: [
+      EventItem(
+        title: "opening ceremony",
+        time: "12:30-13:00",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Kitanodai Gagaku Ensemble",
+        time: "13:00-13:30",
+        duration: 30,
+        image: "",
+      ),
+    ],
     stage2Event: EventItem(
-      title: "Live Music",
-      time: "12:00-12:30",
-      image: "icon_music_mic",
+      title: "set change",
+      time: "12:05-12:15",
+      duration: 10,
+      image: "",
     ),
   ),
-  // 他のスケジュール項目...
+  ScheduleItem(
+    time: "12:30 pm",
+    stage1Events: [
+      EventItem(
+        title: "OKINAWA Ryukyu Kokusai Taiko",
+        time: "13:30-14:00",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: EventItem(
+      title: "Berklee Band",
+      time: "12:55-13:05",
+      duration: 10,
+      image: "",
+    ),
+  ),
+  ScheduleItem(
+    time: "1:00 pm",
+    stage1Events: [
+      EventItem(
+        title: "Jeiko Taiko Ensemble",
+        time: "14:00-14:30",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: EventItem(
+      title: "Anime Band",
+      time: "13:05-13:45",
+      duration: 40,
+      image: "",
+    ),
+  ),
+  ScheduleItem(
+    time: "1:30 pm",
+    stage1Events: [
+      EventItem(
+        title: "Cosplay Fashion Show",
+        time: "14:30-15:00",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Parfait Soleil Dance Performance",
+        time: "15:00-15:20",
+        duration: 20,
+        image: "",
+      ),
+    ],
+    stage2Event: EventItem(
+      title: "Cosplay Fashion Show",
+      time: "13:45-14:05",
+      duration: 20,
+      image: "",
+    ),
+  ),
+  ScheduleItem(
+    time: "2:00 pm",
+    stage1Events: [
+      EventItem(
+        title: "YAMAZAKI VS SUGIMONO",
+        time: "15:20-15:40",
+        duration: 20,
+        image: "",
+      ),
+      EventItem(
+        title: "Japan AirLine Advertising and raffle",
+        time: "15:40-15:50",
+        duration: 10,
+        image: "",
+      ),
+    ],
+    stage2Event: EventItem(
+      title: "parade to Boston Common",
+      time: "14:05-14:25",
+      duration: 20,
+      image: "",
+    ),
+  ),
+  ScheduleItem(
+    time: "2:30 pm",
+    stage1Events: [
+      EventItem(
+        title: "Move&Inspire Kids Dance",
+        time: "15:50-16:10",
+        duration: 20,
+        image: "",
+      ),
+      EventItem(
+        title: "Showa Boston Dance Performance",
+        time: "16:10-16:25",
+        duration: 15,
+        image: "",
+      ),
+      EventItem(
+        title: "Bon Dance Event",
+        time: "16:25-16:55",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: null,
+  ),
 ];
 
 final List<ScheduleItem> day2ScheduleData = [
   ScheduleItem(
     time: "11:00 am",
-    stage1Event: EventItem(
-      title: "Opening Ceremony",
-      time: "11:00-11:30",
-      image: "icon_socialdance", // ※実際のアイコンまたは画像パスに変更
-    ),
+    stage1Events: [
+      EventItem(
+        title: "Opening Ceremony",
+        time: "11:00-11:05",
+        duration: 5,
+        image: "",
+      ),
+      EventItem(
+        title: "Showa Boston Dance Performance",
+        time: "11:05-11:20",
+        duration: 15,
+        image: "",
+      ),
+    ],
     stage2Event: null,
   ),
   ScheduleItem(
-    time: "11:30 am",
-    stage1Event: null,
-    stage2Event: EventItem(
-      title: "Welcome Performance",
-      time: "11:30-11:45",
-      image: "icon_music_note",
-    ),
+    time: "12:00 pm",
+    stage1Events: [
+      EventItem(
+        title: "Kitanodai Gagaku Ensemble",
+        time: "12:00-12:30",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Jeiko Taiko Ensemble",
+        time: "12:30-13:00",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Minami Toyama Noh, Utai, Shinobue",
+        time: "11:20-11:55",
+        duration: 35,
+        image: "",
+      ),
+    ],
+    stage2Event: null,
   ),
   ScheduleItem(
-    time: "12:00 pm",
-    stage1Event: EventItem(
-      title: "Showa Boston Dance Performance",
-      time: "12:00-12:15",
-      image: "icon_dance",
-    ),
-    stage2Event: EventItem(
-      title: "Live Music",
-      time: "12:00-12:30",
-      image: "icon_music_mic",
-    ),
+    time: "11:30 pm",
+    stage1Events: [
+      EventItem(
+        title: "IMPLEMENTATIOIN ENDS HERE",
+        time: "12:00-12:30",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Jeiko Taiko Ensemble",
+        time: "12:30-13:00",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: null,
   ),
-  // 他のスケジュール項目...
+  ScheduleItem(
+    time: "1:00 pm",
+    stage1Events: [
+      EventItem(
+        title: "OKINAWA Ryukyu Kokusai Taiko",
+        time: "13:00-13:30",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "Parfait Soleil Dance Performance",
+        time: "13:30-14:00",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: null,
+  ),
+  ScheduleItem(
+    time: "2:00 pm",
+    stage1Events: [
+      EventItem(
+        title: "Cosplay Fashion Show",
+        time: "14:00-14:30",
+        duration: 30,
+        image: "",
+      ),
+      EventItem(
+        title: "NODO JIMAN",
+        time: "14:30-15:00",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: null,
+  ),
+  ScheduleItem(
+    time: "3:00 pm",
+    stage1Events: [
+      EventItem(
+        title: "Move&Inspire Kids Dance",
+        time: "15:00-15:20",
+        duration: 20,
+        image: "",
+      ),
+      EventItem(
+        title: "Showa Boston Dance Performance",
+        time: "15:20-15:40",
+        duration: 20,
+        image: "",
+      ),
+      EventItem(
+        title: "Bon Dance Event",
+        time: "15:40-16:10",
+        duration: 30,
+        image: "",
+      ),
+    ],
+    stage2Event: null,
+  ),
 ];
