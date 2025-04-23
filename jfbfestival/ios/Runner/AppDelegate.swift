55e127814e80bc37bc7fe74e06f48affa3c68ac6
Soi Hirose <shirose789@gmail.com>
2025-04-22 22:59:14 -0400

    Added more

--- jfbfestival/ios/Runner/AppDelegate.swift
+++ jfbfestival/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import UserNotifications  // Required for push notification registration
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,23 +8,11 @@ import UserNotifications  // Required for push notification registration
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
-    // Register for push notifications
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
-    
+    // try? AVAudioSession.sharedInstance().setCategory(
+    //   .ambient,
+    //   mode: .default,
+    //   options: [.mixWithOthers]
+    // )
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-  // Called when APNs has assigned the device a unique token
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    // Optional: forward token to plugins or your server
-  }
-
-  // Called when APNs registration fails
-  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    print("Failed to register for remote notifications: \(error)")
-  }
 }
-
